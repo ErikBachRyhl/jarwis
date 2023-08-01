0a8928dfcfd5f332b4f29252bcba3b7d32b9fbf1
Erik Bach Ryhl <erik@eriks-mbp.mynet>
2023-08-01 12:57:43 +0200

    File creation, baby

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-import re
+import json
 from dotenv import load_dotenv
 
 # Retrieval
@@ -13,24 +13,13 @@ from langchain.prompts import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 
-# Creating files
-from langchain.tools.file_management import (
-    ReadFileTool,
-    CopyFileTool,
-    DeleteFileTool,
-    MoveFileTool,
-    WriteFileTool,
-    ListDirectoryTool,
-)
-from langchain.agents.agent_toolkits import FileManagementToolkit
-from tempfile import TemporaryDirectory
+from file_creator import generate_files
 
 load_dotenv()
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
 wikipedia = WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper())
-#model="gpt-3.5-turbo-16k-0613"
-llm = ChatOpenAI(openai_api_key=openai_api_key)
+llm = ChatOpenAI(temperature=0, openai_api_key=openai_api_key, model="gpt-3.5-turbo-16k-0613")
 
 template = """You are a helpful research assistant and teacher. 
 I would like you to help me understand how topics relate to each other and the strcuture of knowledge.
@@ -42,154 +31,10 @@ The list should be formatted with the following steps as below:
 2/ Return a nested dictionary which contains the heirarchy of the topics.
 
 Here is an example of a correctly formatted list:
-{{"MAIN TOPIC": {{
-    "SUB TOPIC 1": ["SUB 1 SUB TOPIC 1", "SUB 1 SUB TOPIC 2"],
-    "SUB TOPIC 2": ["SUB 2 SUB TOPIC 1", "SUB 2 SUB TOPIC 2", "SUB 2 SUB TOPIC 3"],
-    "SUB TOPIC 3": ["SUB 3 SUB TOPIC 1"]
-    }}
-}}
+{{"MAIN TOPIC": {{"MAIN TOPIC - SUB TOPIC 1": ["SUB 1 SUB TOPIC 1", "SUB 1 SUB TOPIC 2"], "MAIN TOPIC - SUB TOPIC 2": ["SUB 2 SUB TOPIC 1", "SUB 2 SUB TOPIC 2", "SUB 2 SUB TOPIC 3"], "MAIN TOPIC - SUB TOPIC 3": ["SUB 3 SUB TOPIC 1"]}}}}
 
 Here is an example response on "General Relativity" following above rules:
-{{
-    "General Relativity": {{
-        "Background Knowledge": {{
-            "Special Relativity": [
-                "Lorentz Transformations", 
-                "Time Dilation", 
-                "Length Contraction",
-                "Minkowski Space-Time"
-            ],
-            "Newton's Law of Universal Gravitation": [
-                "Gravity Force",
-                "Gravitational Constant",
-                "Inverse Square Law"
-            ],
-            "Differential Geometry": [
-                "Manifolds", 
-                "Metric Tensor", 
-                "Christoffel Symbols",
-                "Geodesics",
-                "Riemann Curvature Tensor"
-            ],
-            "Tensor Calculus": [
-                "Tensor Basics", 
-                "Contravariant and Covariant Tensors", 
-                "Tensor Operations",
-                "Metric Tensor",
-                "Ricci Curvature"
-            ]
-        }},
-        "Basics of General Relativity": {{
-            "Spacetime": [
-                "Event", 
-                "Worldline", 
-                "Light Cone"
-            ],
-            "Einstein Field Equations": [
-                "Energy-Momentum Tensor", 
-                "Einstein's Field Equations Solutions"
-            ],
-            "Curvature of Spacetime": [
-                "Geodesic Deviation",
-                "Geodetic Effect",
-                "Frame-Dragging"
-            ],
-            "Energy-Momentum Tensor": [
-                "Stress-Energy Tensor",
-                "Perfect Fluid"
-            ]
-        }},
-        "Predictions and Experimental Tests": {{
-            "Gravitational Time Dilation": [
-                "Hafele–Keating Experiment"
-            ],
-            "Gravitational Lensing": [
-                "Einstein Cross",
-                "Einstein Ring"
-            ],
-            "Gravitational Redshift of Light": [
-                "Pound–Rebka Experiment"
-            ],
-            "Shapiro Time Delay": [
-                "Radar Echo Delay Tests"
-            ],
-            "Singularities/Black Holes": [
-                "Event Horizon",
-                "Schwarzschild Radius",
-                "Hawking Radiation",
-                "Interiors of Black Holes"
-            ]
-        }},
-        "Applications in Cosmology": {{
-            "Big Bang Theory": [
-                "Cosmic Inflation", 
-                "Timeline of the Big Bang"
-            ],
-            "Cosmic Microwave Background Radiation": [
-                "Discovery of CMB", 
-                "CMB Spectrum",
-                "CMB Anisotropies"
-            ],
-            "Expansion of the Universe": [
-                "Hubble's Law",
-                "Dark Energy",
-                "Fate of the Universe"
-            ]
-        }},
-        "Astrophysical Implications": {{
-            "Black Holes": [
-                "Schwarzschild Black Holes",
-                "Kerr Black Holes",
-                "Black Hole Thermodynamics"
-            ],
-            "Microquasars and Active Galactic Nuclei": [
-                "Accretion Disk",
-                "Astrophysical Jets"
-            ],
-            "Gravitational Waves": [
-                "Detection by LIGO",
-                "Gravitational Wave Astronomy",
-                "Sources of Gravitational Waves"
-            ],
-            "Gravitational Lensing": [
-                "Strong Lensing",
-                "Weak Lensing",
-                "Microlensing"
-            ],
-            "Expanding Universe Models": [
-                "Friedmann-Lemaître-Robertson-Walker metric",
-                "Observational Evidence"
-            ]
-        }},
-        "Unresolved Issues": {{
-            "Quantum Gravity": [
-                "String Theory",
-                "Loop Quantum Gravity"
-            ],
-            "Unification with Other Fundamental Forces": [
-                "Grand Unified Theory",
-                "Theory of Everything"
-            ]
-        }},
-        "Philosophical and Theoretical Implications": {{
-            "Concept of Time": [
-                "Arrow of Time",
-                "Block Universe"
-            ],
-            "Concept of Space": [
-                "Absolute vs Relative Space"
-            ],
-            "The Nature of Gravity": [
-                "Problem of Time",
-                "Quantum Gravity"
-            ],
-            "Interpretations of General Relativity": [
-                "Hole Argument",
-                "Einstein's Equivalence Principle"
-            ]
-        }}
-    }}
-}}
+{{"General Relativity": {{"General Relativity - Background Knowledge": {{"Special Relativity": ["Lorentz Transformations", "Time Dilation", "Length Contraction", "Minkowski Space-Time"], "Newton's Law of Universal Gravitation": ["Gravity Force", "Gravitational Constant", "Inverse Square Law"], "Differential Geometry": ["Manifolds", "Metric Tensor", "Christoffel Symbols", "Geodesics", "Riemann Curvature Tensor"], "Tensor Calculus": ["Tensor Basics", "Contravariantand Covariant Tensors", "Tensor Operations", "Metric Tensor", "Ricci Curvature"]}}, "General Relativity - Basic Concepts": {{"Spacetime": ["Event", "Worldline", "Light Cone"], "Einstein Field Equations": ["Energy-Momentum Tensor", "Einstein's Field Equations Solutions"], "Curvature of Spacetime": ["Geodesic Deviation", "Geodetic Effect", "Frame-Dragging"], "Energy-Momentum Tensor": ["Stress-Energy Tensor", "Perfect Fluid"]}}, "General Relativity - Predictions and Experimental Tests": {{"GravitationalTimeDilation": ["Hafele–Keating Experiment"], "Gravitational Lensing": ["Einstein Cross", "Einstein Ring"], "Gravitational Redshift of Light": ["Pound–Rebka Experiment"], "Shapiro Time Delay": ["Radar Echo Delay Tests"], "Singularities and Black Holes": ["Event Horizon", "Schwarzschild Radius", "Hawking Radiation", "Interiors of Black Holes"]}}, "General Relativity - Applications in Cosmology": {{"Big Bang Theory": ["Cosmic Inflation", "Timeline of the Big Bang"], "Cosmic Microwave Background Radiation": ["Discovery of CMB", "CMB Spectrum", "CMB Anisotropies"], "Expansion of the Universe": ["Hubble's Law", "Dark Energy", "Fate of the Universe"]}}, "General Relativity - Astrophysical Implications": {{"Black Holes": ["Schwarzschild Black Holes", "Kerr Black Holes", "Black Hole Thermodynamics"], "Microquasars and Active Galactic Nuclei": ["Accretion Disk", "Astrophysical Jets"], "Gravitational Waves": ["Detection by LIGO", "Gravitational Wave Astronomy", "Sources of Gravitational Waves"], "Gravitational Lensing": ["Strong Lensing", "Weak Lensing", "Microlensing"], "Expanding Universe Models": ["Friedmann-Lemaître-Robertson-Walker metric", "Observational Evidence"]}}, "General Relativity - Unresolved Issues": {{"Quantum Gravity": ["String Theory", "Loop Quantum Gravity"], "Unification with Other Fundamental Forces": ["Grand Unified Theory", "Theory of Everything"]}}, "General Relativity - Philosophical and Theoretical Implications": {{"Concept of Time": ["Arrow of Time", "Block Universe"], "Concept of Space": ["Absolute vs Relative Space"], "The Nature of Gravity": ["Problem of Time", "Quantum Gravity"], "Interpretations of General Relativity": ["Hole Argument", "Einstein's Equivalence Principle"]}}}}}}
 
 Here is your topic name: {topic}
 Wikipedia exerpt on {topic}:
@@ -198,19 +43,6 @@ Wikipedia exerpt on {topic}:
 Begin!
 """
 
-# def extract_topics(text):
-#     # Regular expression pattern for matching the topic syntax
-#     pattern = re.compile(r'\[\[(.*?)(?:\|(.*?))?\]\]')
-    
-#     # Find all matches in the text
-#     matches = pattern.findall(text)
-    
-#     # Extract the topic names and add them to the list
-#     topics = [match[0] for match in matches]
-    
-#     return topics
-
-# Put together prompt
 prompt = PromptTemplate(
     input_variables=["topic", "wiki_info"], 
     template=template
@@ -221,47 +53,28 @@ chain = LLMChain(
     llm=llm,
     prompt=prompt
 )
-
-name = "Quantum Mechanics"
     
 def generate_response(topic_name):
     wiki_info = wikipedia.run(topic_name)
     response = chain.run(topic=topic_name, wiki_info=wiki_info)
     return response
 
-#response = generate_response(name)
+raw_response = generate_response("Classical Mechanics")
 
-# Temporary directory for llm
-working_directory = TemporaryDirectory()
-file_management_toolkit = FileManagementToolkit(root_dir=working_directory.name)
+def is_valid_json(json_string):
+    try:
+        json.loads(json_string.strip())
+    except ValueError as e:
+        print("Error loading response as JSON. Error:", str(e))
+        return False
+    return True
 
-tools = FileManagementToolkit(
-    root_dir=str(working_directory.name),
-    selected_tools=["read_file", "write_file", "list_directory"],
-).get_tools()
+if is_valid_json(raw_response):
+    try: 
+        response = json.loads(raw_response.strip())
+        generate_files(response)
+    except ValueError:
+        print("Error running 'generate_files'. Error:", str(e))
+else:
+    print("The reponse from the LLM couldn't be parsed properly: Raw Response from LLM:\n\n", raw_response)
 
-read_tool, write_tool, list_tool = tools
-
-def generate_files(response):
-
-    def generate_md_files(name, content):
-        if isinstance(content, dict):  # If content is a dictionary
-            md_content = ", ".join(f"[[{k}]]" for k in content)
-            for k, v in content.items():
-                generate_md_files(k, v)
-        elif isinstance(content, list):  # If content is a list
-            md_content = "\n".join(f"[[{item}]]" for item in content)
-        else:
-            return
-
-        # Write the content to the markdown file
-        write_tool.run({"file_path": f"{name}.md", "text": md_content})
-
-    # Start the process from the root of the response
-    for k, v in response.items():
-        generate_md_files(k, v)
-
-
-response = generate_response("Quantum Mechanics")
-print(response)
-generate_files(response)
